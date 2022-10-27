db14de1a024b40206cd26713606a1494dceab03e
Stefan <satalbot@protonmail.com>
2022-10-27 22:25:00 +0000

    Fixed lint errors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { nextISSTimesForMyLocation} = require("./iss");
 
 nextISSTimesForMyLocation((error, passTimes) => {
-  if(error) {
+  if (error) {
     throw error;
   }
 
   passTimes.forEach((passTime) => {
-    console.log(`Next pass at ${new Date(passTime.risetime * 1000)} for ${passTime.duration} seconds!`) // We have to multiple our risttimes by 1000 because linux??
+    console.log(`Next pass at ${new Date(passTime.risetime * 1000)} for ${passTime.duration} seconds!`); // We have to multiple our risttimes by 1000 because linux??
   });
 });
